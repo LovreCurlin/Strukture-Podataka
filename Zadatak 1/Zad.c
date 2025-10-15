1ff311ee0315ae0b54150e05b3ef3e2963474be4
LovreCurlin@github.com <lovre.curlin10@gmail.com>
2025-10-15 20:54:53 +0200

    Zadatak 1 with comments

--- Zadatak 1/Zad.c
+++ Zadatak 1/Zad.c
@@ -9,65 +9,61 @@ relatvan_br_bodova = br_bodova / max_br_bodova * 100
 
 #include<stdio.h>
 #include<stdlib.h>
-#define maxPoints 60
+#define maxPoints 60       // maksimalan broj bodova na kolokviju
 
-typedef struct {
-
-    char firstName[20];
-    char lastName[10];
-    int score;
-
+typedef struct {           // definicija strukture student
+    char firstName[20];    // string ime studenta
+    char lastName[20];     // string prezime studenta
+    int score;             // intiger broj bodova koje je student ostvario
 } student;
 
-int numberOfStudents(FILE* );
-int enterOfStudents(FILE*, student*, int);
-float relativeScore(int);
+int numberOfStudents(FILE* );                     // prototip funkcije koja prebrojava koliko je studenata na listi
+int importStudents(FILE*, student*, int);         // prototip funkcije koja upisuje podatke iz datoteke u strukturu
+float relativeScore(int);                         // prototip funkcije koja racuna relativni broj bodova
 
 int main()
 {
 
     student* s;
-    int i;
-    FILE* listOfStudents = fopen("Students.txt", "r");
+    int i;                                                           
+    FILE* listOfStudents = fopen("Students.txt", "r");               // otvaranje datoteke za citanje
 
-    int numberOfRows = numberOfStudents(listOfStudents);
+    int numberOfRows = numberOfStudents(listOfStudents);             // poziv funkcije koja vraca broj studenata na listi
 
-    s = malloc(numberOfRows * sizeof(student));
+    s = malloc(numberOfRows * sizeof(student));                      // dinamicki alociramo memoriju za s
 
-    rewind(listOfStudents);
-    enterOfStudents(listOfStudents, s, numberOfRows);
+    rewind(listOfStudents);                                          // vracamo pokazivac na pocetak datoteke
+    importStudents(listOfStudents, s, numberOfRows);                 // poziv funkcije koja upisuje podatke iz datoteke u strukturu
 
     for (i = 0; i < numberOfRows; i++)
-        printf("%-10s %s \t\t%d   %.2f\n", s[i].firstName, s[i].lastName, s[i].score, relativeScore(s[i].score));
+        printf("%-10s %s \t\t%d   %.2f\n", s[i].firstName, s[i].lastName, s[i].score, relativeScore(s[i].score));   // ispis elemenata strukture
 
-    free(s);
+    free(s);     // oslobadanje alociranu memoriju                                                     
 
     return 0;
 }
 
-int numberOfStudents(FILE* listOfStudents)
+int numberOfStudents(FILE* listOfStudents)     // funkcija koja preborjava studente
 {
     char string[100];
     int count = 0;
 
-    while (fgets(string, 100, listOfStudents))
-        count++;
+    while (fgets(string, 100, listOfStudents)) // cita svaku liniju iz datotek dok ne dode do kraja
+        count++;                               // brojaca koji broji svaki red
 
-    return count;
+    return count;                              // funkcija vraca broj redova(studenata)
 }
 
-int enterOfStudents(FILE* listOfStudents, student* s, int numberOfRows)
+int importStudents(FILE* listOfStudents, student* s, int numberOfRows)                       // funkcije koja upisuje podatke iz datoteke u strukturu
 {
     int i;
     for (i = 0; i < numberOfRows; i++)
-        fscanf(listOfStudents, "%s %s %d", s[i].firstName, s[i].lastName, &s[i].score);
+        fscanf(listOfStudents, "%s %s %d", s[i].firstName, s[i].lastName, &s[i].score);      // upis podataka u strukturu
 
     return 0;
-
-
 }
 
-float relativeScore(int score)
+float relativeScore(int score)               //funkcija koja racuna relativni broj bodova
 {
-    return (float)score / maxPoints * 100;
+    return (float)score / maxPoints * 100;   // funkcija vraca rezultat tipa float 
 }
